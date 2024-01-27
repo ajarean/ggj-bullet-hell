4d291d10710bdc1e2d7c83da5c6e1f9bf133035f
zenxha <komays5042@gmail.com>
2024-01-27 14:27:18 -0800

    player movement unity input manager

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        // _rigidbody.velocity = new Vector2(1, 0.5f); // 1 unit per second on the x axis, 0.5 on y
+        _rigidbody.velocity = _movementInput * _speed;
+    }
+
+    private void OnMove(InputValue inputValue) {
+        Vector2 vector = inputValue.Get<Vector2>();
+        _movementInput = vector;
+        Debug.Log("input detected. Magnitude(" + vector.x + ", " + vector.y);
+
+    }
+}
+
