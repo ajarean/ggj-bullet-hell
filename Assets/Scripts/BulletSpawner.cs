f39be13938d06bfc24223bc79eebfacd4bd48d49
ajarean <117534525+ajarean@users.noreply.github.com>
2024-01-27 21:39:55 -0800

    randomized firing modes (hard coded for now)

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -10,33 +10,35 @@ public class BulletSpawner : MonoBehaviour
 
     [Header("Bullet Attributes")]
     public GameObject bullet;
-    public float bulletLife = 1f;
-    public float speed = 1f; 
+    public float bulletLife = 5f; //DONT CHANGE THIS
+    public float speed = 5f; //DONT CHANGE THIS 
 
     [Header("Spawner Attributes")]
     [SerializeField] private SpawnerType spawnerType;
-    [SerializeField] private float fireRate = 1f;
-    [SerializeField] private int numberOfBullets;
-    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float fireRate = 1f; //2 for straight, 0.5-1 for others
+    [SerializeField] private int numberOfBullets; //3-4 for straight, 8-10 for spin, 3-6 for spread
+    [SerializeField] private float rotationSpeed = 1f; //i put it as 0.2 in the inspector
 
-    [SerializeField] private int firingTime = 20;
+    [SerializeField] private int firingTime = 5; //time interval between firing modes
 
     private GameObject spawnedBullet;
     private float timer = 0f;
     private float firingTimer = 0f;
     private int timeInSeconds;
     // Start is called before the first frame update
-    void Start()
-    {
-        SpawnerState = SpawnerType.Straight;
+    void Start(){
+        //SpawnerState = SpawnerType.Straight;
+        // SpawnerState = (SpawnerType)Random.Range(0, Enum.GetNames(typeof(SpawnerType)).Length);
+        SpawnerState = (SpawnerType)Random.Range(1, 4);
+        
         // Debug.Log((int)SpawnerType.init); 0
         // Debug.Log((int)SpawnerType.Straight); 1
         // Debug.Log((int)SpawnerType.Spin); 2
+        Debug.Log((int)SpawnerType.Spread);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         timer += Time.deltaTime;
         if(SpawnerState == SpawnerType.Spin){
             transform.eulerAngles = new Vector3(0f,0f,transform.eulerAngles.z+rotationSpeed);
@@ -62,37 +64,42 @@ public class BulletSpawner : MonoBehaviour
         //Debug.Log(firingTimer%60); //THIS GIVES TIME IN SECONDS
         if(firingTimer%60 >= firingTime){
             //assign a new SpawnerType
-            //Debug.Log("TIME TO SWITCH");
+            // SpawnerState = (SpawnerType)Random.Range(0, Enum.GetValues(typeof(SpawnerType)).Length);
+            SpawnerState = (SpawnerType)Random.Range(1, 4);
+            firingTimer = 0;
         }
 
+    }
+
+    private IEnumerator FireStraight(float burstTime){
+        //burst fire, num bullets is burst count
+        // Debug.Log("firestraight called");
+        WaitForSeconds wait = new WaitForSeconds(burstTime);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        for(int i = 0; i< numberOfBullets; i++){
+            spawnedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, CalculateAngleToMouse(mousePosition)));
+            spawnedBullet.GetComponent<Bullet>().speed = speed;
+            spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
+            yield return wait;
+        }
     }
 
     private void FireSpin(){
         for(int i=0;i < numberOfBullets; i++){
             if(bullet){
                 float angle = i * (360f / numberOfBullets);
-                // Debug.Log("angle " + i + " is " + angle);
-                // spawnedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle));
-                // spawnedBullet.GetComponent<Bullet>().speed = speed;
-                // spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
-                // spawnedBullet.transform.rotation = transform.rotation;
                 spawnedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle + transform.eulerAngles.z));;
-        
-                //Bullet bulletScript = spawnedBullet.GetComponent<Bullet>();
                 spawnedBullet.GetComponent<Bullet>().speed = speed;
                 spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
             }
         }
     }
 
-    private void FireSpread()
-    {
+    private void FireSpread(){
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
-        for (int i = 0; i < numberOfBullets; i++)
-        {
-            if (bullet)
-            {
+        for (int i = 0; i < numberOfBullets; i++){
+            if (bullet){
                 float angle;
                 if(numberOfBullets % 2 == 0){
                     angle = CalculateAngleToMouse(mousePosition) + (i-numberOfBullets/2) * 10f + 5f;
@@ -100,7 +107,6 @@ public class BulletSpawner : MonoBehaviour
                 else{
                     angle = CalculateAngleToMouse(mousePosition) + (i-numberOfBullets/2) * 10f;     
                 }
-                 // Adjust the angle increment as needed
                 spawnedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle));
                 spawnedBullet.GetComponent<Bullet>().speed = speed;
                 spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
@@ -108,24 +114,8 @@ public class BulletSpawner : MonoBehaviour
         }
     }
 
-    private float CalculateAngleToMouse(Vector3 mousePosition)
-    {
+    private float CalculateAngleToMouse(Vector3 mousePosition){
         Vector2 direction = mousePosition - transform.position;
         return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-    }
-
-    private IEnumerator FireStraight(float burstTime){
-        //burst fire, num bullets is burst count
-        Debug.Log("firestraight called");
-        WaitForSeconds wait = new WaitForSeconds(burstTime);
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        for(int i = 0; i< numberOfBullets; i++){
-            spawnedBullet = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, CalculateAngleToMouse(mousePosition)));
-            spawnedBullet.GetComponent<Bullet>().speed = speed;
-            spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
-            //yield return new WaitForSeconds(burstTime);
-            yield return wait;
-        }
-        //this spawns them all in a stack, we need to wait 
     }
 }
